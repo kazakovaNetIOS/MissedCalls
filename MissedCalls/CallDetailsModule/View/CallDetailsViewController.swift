aed5cd6d3138ad2b611dd3c5166013c3163d169f
KazakovaNetIOS <kazakova.net@yandex.ru>
2021-01-28 04:12:05 +0300

    Layout business number labels on detail screen

--- MissedCalls/CallDetailsModule/View/CallDetailsViewController.swift
+++ MissedCalls/CallDetailsModule/View/CallDetailsViewController.swift
@@ -11,13 +11,18 @@ class CallDetailsViewController: UIViewController {
   @IBOutlet weak var nameLabel: UILabel!
   @IBOutlet weak var phoneLabel: UILabel!
   @IBOutlet weak var durationLabel: UILabel!
-
+  @IBOutlet weak var businessNumberTitleLabel: UILabel!
+  @IBOutlet weak var businessNumberLabel: UILabel!
+  @IBOutlet weak var foldButton: UIView!
+  @IBOutlet weak var wrappedView: UIView!
+  
   var viewModel: CallDetailsViewModelProtocol?
 
   override func viewDidLoad() {
     super.viewDidLoad()
 
-    navigationController?.title = "Missed call"
+    setupViews()
+
     viewModel?.updateViewData = { [weak self] viewData in
       self?.updateView(with: viewData)
     }
@@ -28,10 +33,24 @@ class CallDetailsViewController: UIViewController {
 // MARK: - Private
 
 private extension CallDetailsViewController {
+  func setupViews() {
+    navigationController?.title = "Missed call"
+    foldButton.layer.cornerRadius = 2
+    foldButton.clipsToBounds = true
+
+    wrappedView.layer.cornerRadius = 16
+    wrappedView.layer.shadowColor = UIColor.black.cgColor
+    wrappedView.layer.shadowOpacity = 0.05
+    wrappedView.layer.shadowOffset = .zero
+    wrappedView.layer.shadowRadius = 16
+  }
+
   func updateView(with model: Request) {
     nameLabel.text = model.client?.name
     phoneLabel.text = model.client?.address
     durationLabel.text = model.duration
+    businessNumberTitleLabel.text = model.businessNumber?.label
+    businessNumberLabel.text = model.businessNumber?.number
   }
 }
 
